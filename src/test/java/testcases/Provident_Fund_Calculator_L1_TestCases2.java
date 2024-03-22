4c04afabb79127c462ad3930779d5f1f1ba191e7
invincixziksha <incixziksha@gmail.com>
2024-03-23 01:04:03 +0530

    Intial commit of Fixed deposit Interest and Provident Fund Calculator page

---
+++ src/test/java/testcases/Provident_Fund_Calculator_L1_TestCases2.java
@@ -0,0 +1,134 @@
+
+
+package testcases;
+
+import java.util.Map;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import coreUtilities.testutils.ApiHelper;
+import coreUtilities.utils.FileOperations;
+import pages.Fixed_Deposit_Interest_Calculator_L1_Pages;
+import pages.Provident_Fund_Calculator_L1_Pages2;
+import pages.StartupPage;
+import testBase.AppTestBase;
+import testdata.LocatorsFactory;
+
+public class Provident_Fund_Calculator_L1_TestCases2 extends AppTestBase {
+	
+	Map<String, String> configData;
+	Map<String, String> loginCredentials;
+	String expectedDataFilePath = testDataFilePath+"expected_data.json";
+	StartupPage startupPage;
+	Provident_Fund_Calculator_L1_Pages2 providentFundCalculatorPageInstance;
+	LocatorsFactory LocatorsFactoryInstance=new LocatorsFactory(driver);
+	
+	@Parameters({"browser", "environment"})
+	@BeforeClass(alwaysRun = true)
+	public void initBrowser(String browser, String environment) throws Exception {
+		configData = new FileOperations().readJson(config_filePath, environment);
+		configData.put("url", configData.get("url").replaceAll("[\\\\]", ""));
+		configData.put("browser", browser);
+		
+		boolean isValidUrl = new ApiHelper().isValidUrl(configData.get("url"));
+		Assert.assertTrue(isValidUrl, configData.get("url")+" might be Server down at this moment. Please try after sometime.");
+		initialize(configData);
+		startupPage = new StartupPage(driver);
+	}
+	
+	
+	@Test(priority = 1, groups = {"sanity"}, description="Navigate to “Fixed Deposit Interest Calculator” sub-menu from “Personal Finance” Menu.")
+	public void navigateToProvidentFundCalculatorPage() throws Exception {
+		providentFundCalculatorPageInstance = new Provident_Fund_Calculator_L1_Pages2(driver);
+		Map<String, String> expectedData = new FileOperations().readJson(expectedDataFilePath, "MoneyControlPageTitle");
+		Assert.assertEquals(providentFundCalculatorPageInstance.validateTitleOfCurrentPage(), expectedData.get("HomepageTitle"));
+		Assert.assertEquals(providentFundCalculatorPageInstance.navigateToProvidentFundCalculatorPage(),expectedData.get("ProvidentFundCalculatorPageTitle"));
+		Assert.assertTrue(LocatorsFactoryInstance.providentFundCalculatorHeader(driver).isDisplayed(), "provident fund Calculator Header is not present in the current page, Please check manually");
+	}	
+	
+	@Test(priority = 2, groups = {"sanity"}, description="Enter basic monthly salary.")
+	public void enterBasicMonthlySalaryInProvidentFundCalculatorForm() throws Exception {
+		providentFundCalculatorPageInstance = new Provident_Fund_Calculator_L1_Pages2(driver);
+		Map<String, String> expectedData = new FileOperations().readJson(expectedDataFilePath, "providentFundCalculatorForm");
+		Assert.assertEquals(providentFundCalculatorPageInstance.enterBasicMonthlySalaryInProvidentFundCalculatorForm(expectedData), expectedData.get("yourBasicSalaryMonthly"));
+		Assert.assertTrue(LocatorsFactoryInstance.providentFundCalculatorHeader(driver).isDisplayed(), "provident Fund Calculator Header element is not present in the current page, Please check manually");
+	}
+	
+	@Test(priority = 3, groups = {"sanity"}, description="Enter your contribution to EPF.")
+	public void enterValueInYourContributionToEPFInProvidentFundCalculatorForm() throws Exception {
+		providentFundCalculatorPageInstance = new Provident_Fund_Calculator_L1_Pages2(driver);
+		Map<String, String> expectedData = new FileOperations().readJson(expectedDataFilePath, "providentFundCalculatorForm");
+		Assert.assertEquals(providentFundCalculatorPageInstance.enterValueInYourContributionToEPFInProvidentFundCalculatorForm(expectedData), expectedData.get("contributioToEPF"));
+		Assert.assertTrue(LocatorsFactoryInstance.providentFundCalculatorHeader(driver).isDisplayed(), "provident Fund Calculator Header element is not present in the current page, Please check manually");
+	}
+	
+	@Test(priority = 4, groups = {"sanity"}, description="Enter your employers contribution to EPF.")
+	public void enterYourEmployersContributionToEPFInProvidentFundCalculatorForm() throws Exception {
+		providentFundCalculatorPageInstance = new Provident_Fund_Calculator_L1_Pages2(driver);
+		Map<String, String> expectedData = new FileOperations().readJson(expectedDataFilePath, "providentFundCalculatorForm");
+		Assert.assertEquals(providentFundCalculatorPageInstance.enterYourEmployersContributionToEPFInProvidentFundCalculatorForm(expectedData), expectedData.get("employersContributionToEPF"));
+		Assert.assertTrue(LocatorsFactoryInstance.providentFundCalculatorHeader(driver).isDisplayed(), "provident Fund Calculator Header element is not present in the current page, Please check manually");
+	}
+	
+	@Test(priority = 5, groups = {"sanity"}, description="Enter average increase in salary you expect.")
+	public void enterAvgIncreaseInSalaryYouExpectcInProvidentFundCalculatorForm() throws Exception {
+		providentFundCalculatorPageInstance = new Provident_Fund_Calculator_L1_Pages2(driver);
+		Map<String, String> expectedData = new FileOperations().readJson(expectedDataFilePath, "providentFundCalculatorForm");
+		Assert.assertEquals(providentFundCalculatorPageInstance.enterAvgIncreaseInSalaryYouExpectInProvidentFundCalculatorForm(expectedData), expectedData.get("avgIncreaseInSalaryYouExpect"));
+		Assert.assertTrue(LocatorsFactoryInstance.providentFundCalculatorHeader(driver).isDisplayed(), "provident Fund Calculator Header element is not present in the current page, Please check manually");
+	}
+	
+	@Test(priority = 6, groups = {"sanity"}, description="Enter age when you intend to retire.")
+	public void enterAgeWhenYouIntendToRetireInProvidentFundCalculatorForm() throws Exception {
+		providentFundCalculatorPageInstance = new Provident_Fund_Calculator_L1_Pages2(driver);
+		Map<String, String> expectedData = new FileOperations().readJson(expectedDataFilePath, "providentFundCalculatorForm");
+		Assert.assertEquals(providentFundCalculatorPageInstance.enterAgeWhenYouIntendToRetireInProvidentFundCalculatorForm(expectedData), expectedData.get("ageWhenYouIntendToRetire"));
+		Assert.assertTrue(LocatorsFactoryInstance.providentFundCalculatorHeader(driver).isDisplayed(), "provident Fund Calculator Header element is not present in the current page, Please check manually");
+	}
+	
+	@Test(priority = 7, groups = {"sanity"}, description="Enter current EPF balance.")
+	public void enterCurrentEpfBalanceIfAnyInProvidentFundCalculatorForm() throws Exception {
+		providentFundCalculatorPageInstance = new Provident_Fund_Calculator_L1_Pages2(driver);
+		Map<String, String> expectedData = new FileOperations().readJson(expectedDataFilePath, "providentFundCalculatorForm");
+		Assert.assertEquals(providentFundCalculatorPageInstance.enterCurrentEpfBalanceIfAnyInProvidentFundCalculatorForm(expectedData), expectedData.get("currentEpfBalanceIfAny"));
+		Assert.assertTrue(LocatorsFactoryInstance.providentFundCalculatorHeader(driver).isDisplayed(), "provident Fund Calculator Header element is not present in the current page, Please check manually");
+	}
+	
+	@Test(priority = 8, groups = {"sanity"}, description="Enter current interest rate.")
+	public void enterCurrentInterestRateInProvidentFundCalculatorForm() throws Exception {
+		providentFundCalculatorPageInstance = new Provident_Fund_Calculator_L1_Pages2(driver);
+		Map<String, String> expectedData = new FileOperations().readJson(expectedDataFilePath, "providentFundCalculatorForm");
+		Assert.assertEquals(providentFundCalculatorPageInstance.enterCurrentInterestRateInProvidentFundCalculatorForm(expectedData), expectedData.get("currentInterestRate"));
+		Assert.assertTrue(LocatorsFactoryInstance.providentFundCalculatorHeader(driver).isDisplayed(), "provident Fund Calculator Header element is not present in the current page, Please check manually");
+	}
+	
+	@Test(priority = 9, groups = {"sanity"}, description="Enter current interest rate.")
+	public void clickOnCalculateButtonInProvidentFundCalculatorForm() throws Exception {
+		providentFundCalculatorPageInstance = new Provident_Fund_Calculator_L1_Pages2(driver);
+		Assert.assertTrue(providentFundCalculatorPageInstance.clickOnCalculateButtonInProvidentFundCalculatorForm(), "calculate button is not present, please check manually");
+		Assert.assertTrue(LocatorsFactoryInstance.providentFundCalculatorHeader(driver).isDisplayed(), "provident Fund Calculator Header element is not present in the current page, Please check manually");
+	}
+	
+	@Test(priority = 10, groups = {"sanity"}, description="Fetch the result.")
+	public void fetchTotalAmountByTheTimeYouRetire() throws Exception {
+		providentFundCalculatorPageInstance = new Provident_Fund_Calculator_L1_Pages2(driver);
+		Assert.assertTrue(providentFundCalculatorPageInstance.fetchTotalAmountByTheTimeYouRetire(), "total payment result button is not present, please check manually");
+		Assert.assertTrue(LocatorsFactoryInstance.providentFundCalculatorHeader(driver).isDisplayed(), "provident Fund Calculator Header element is not present in the current page, Please check manually");
+	}
+	
+	@AfterClass(alwaysRun = true)
+	public void tearDown() {
+		System.out.println("before closing the browser");
+		browserTearDown();
+	}
+	
+	@AfterMethod
+	public void retryIfTestFails() throws Exception {
+		startupPage.navigateToUrl(configData.get("url"));
+	}
+}
--- src/test/java/testdata/LocatorsFactory.java
+++ src/test/java/testdata/LocatorsFactory.java
@@ -9,7 +9,23 @@ import pages.StartupPage;
 public class LocatorsFactory extends StartupPage {
 	//please write all the locators for Money Control page only 
 
-	By refreshButton = By.xpath("//button[@value='Refresh']"); 
+	By fixedDepositCalculatorHeader = By.xpath("//h2[contains(text(), 'Fixed Deposit Calculator')]");
+	By investmentAmountTextfield = By.xpath("//input[@class='valueinput']");
+	By investmentPeriodTextfield = By.xpath("//input[@id='edulonvalue_2']");
+	By rateOfReturnTextfield = By.xpath("//input[@id='edulonvalue_3']");
+	By interestFrequencyYearlyButton = By.xpath("(//span[contains(text(), 'Yearly')])[2]");
+	By taxRateTextfield = By.xpath("//input[@id='edulonvalue_4']");
+	By submitButton = By.xpath("//a[contains(text(), 'Submit')]");
+	By resetButton = By.xpath("//a[@id='reset_btn']");
+	By totalInterestResultButton = By.xpath("//div[contains(text(), 'Total Interest')]");
+	By totalPaymentResultButton = By.xpath("//div[contains(text(), 'Total Payment')]");
+	By totalCorpusResultButton = By.xpath("//div[contains(text(), 'Total Corpus')]");
+	By totalTaxAmountResultButton = By.xpath("//div[contains(text(), 'Post Tax Amount')]");
+	
+	
+	
+	
+	By providentFundCalculatorHeader = By.xpath("//strong[contains(text(), 'PF - Provident Fund Calculator')]"); 
 	
 
 	public LocatorsFactory(WebDriver driver) {
@@ -22,9 +38,71 @@ public class LocatorsFactory extends StartupPage {
 
 
 	//Expected data will be fetch from config.json file,path of the json file is:FaceBook_Automation_Dummy/src/main/resources/config.json
-
-	public WebElement refreshButton(WebDriver driver) {
-		WebElement refreshButtonWebElement = driver.findElement(refreshButton);
-		return refreshButtonWebElement; 
+	
+	public WebElement fixedDepositCalculatorHeader(WebDriver driver) {
+		WebElement fixedDepositCalculatorHeaderWebElement = driver.findElement(fixedDepositCalculatorHeader);
+		return fixedDepositCalculatorHeaderWebElement; 
 	}
+	
+	public WebElement investmentAmountTextfield(WebDriver driver) {
+		WebElement investmentAmountTextfieldWebElement = driver.findElement(investmentAmountTextfield);
+		return investmentAmountTextfieldWebElement; 
+	}
+	
+	public WebElement investmentPeriodTextfield(WebDriver driver) {
+		WebElement investmentPeriodTextfieldWebElement = driver.findElement(investmentPeriodTextfield);
+		return investmentPeriodTextfieldWebElement; 
+	}
+	
+	public WebElement rateOfReturnTextfield(WebDriver driver) {
+		WebElement rateOfReturnTextfieldWebElement = driver.findElement(rateOfReturnTextfield);
+		return rateOfReturnTextfieldWebElement; 
+	}
+	
+	public WebElement interestFrequencyYearlyButton(WebDriver driver) {
+		WebElement interestFrequencyYearlyButtonWebElement = driver.findElement(interestFrequencyYearlyButton);
+		return interestFrequencyYearlyButtonWebElement; 
+	}
+	
+	public WebElement taxRateTextfield(WebDriver driver) {
+		WebElement taxRateTextfieldWebElement = driver.findElement(taxRateTextfield);
+		return taxRateTextfieldWebElement; 
+	}
+	
+	public WebElement submitButton(WebDriver driver) {
+		WebElement submitButtonWebElement = driver.findElement(submitButton);
+		return submitButtonWebElement; 
+	}
+	
+	public WebElement resetButton(WebDriver driver) {
+		WebElement resetButtonWebElement = driver.findElement(resetButton);
+		return resetButtonWebElement; 
+	}
+	
+	public WebElement totalInterestResultButton(WebDriver driver) {
+		WebElement totalInterestResultButtonWebElement = driver.findElement(totalInterestResultButton);
+		return totalInterestResultButtonWebElement; 
+	}
+	
+	public WebElement totalPaymentResultButton(WebDriver driver) {
+		WebElement totalPaymentResultButtonWebElement = driver.findElement(totalPaymentResultButton);
+		return totalPaymentResultButtonWebElement; 
+	}
+	
+	public WebElement totalCorpusResultButton(WebDriver driver) {
+		WebElement totalCorpusResultButtonWebElement = driver.findElement(totalCorpusResultButton);
+		return totalCorpusResultButtonWebElement;
+	}
+	
+	public WebElement totalTaxAmountResultButton(WebDriver driver) {
+		WebElement totalTaxAmountResultButtonWebElement = driver.findElement(totalTaxAmountResultButton);
+		return totalTaxAmountResultButtonWebElement;
+	}
+	
+	public WebElement providentFundCalculatorHeader(WebDriver driver) {
+		WebElement providentFundCalculatorHeaderWebElement = driver.findElement(providentFundCalculatorHeader);
+		return providentFundCalculatorHeaderWebElement; 
+	}
+	
+	
 }
